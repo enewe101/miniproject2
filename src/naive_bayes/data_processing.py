c3f8ab33decd9dbfa59fba81f3aad65d3b9c91e8
Edward Newell <edward.newell@gmail.com>
2014-10-07 16:34:12 -0400

    add tficf

--- src/naive_bayes/data_processing.py
+++ src/naive_bayes/data_processing.py
@@ -41,10 +41,15 @@ class Data(object):
 		'read':'data/raw/train.csv',
 		'as_frequencies': 'data/cache/frequencies.json',
 		'lemmatized': 'data/cache/lemmatized.json',
+		'hypernyms': 'data/cache/hypernyms.json',
+		'tficf': 'data/cache/tficf.json',
+		'class_counts': 'data/cache/class_counts.json',
+		'icf_scores': 'data/cache/icf_scores.json',
 	}
 
 	# Keep things in cache so we don't need to keep calculating
 	CACHE = {}
+
 
 	# Some flags
 	NOT_CACHED = 0
@@ -130,6 +135,106 @@ class Data(object):
 		self.cache('lemmatized', return_data)
 
 		return return_data
+
+
+	def hypernyms(self, use_cache=True):
+		'''
+		uses a frequency-representation (see `self.as_frequencies`).
+		we include the hypernyms of words (using wordnet)
+		'''
+		# check for cached data first
+		if use_cache and self.check_cache('hypernyms'):
+			return self.get_cache('hypernyms')
+
+		w = nltk.corpus.wordnet
+
+		return_data = []
+		for row in self.data:
+
+			# split on any series of characters that aren't letters, 
+			# numbers or the underscore
+			features = self.NON_ALPHANUM.split(row[1])
+
+			# get the synsets, then get their hypernyms
+			synsets = reduce(lambda x,y: x + w.synsets(y), features, [])
+			hypernyms = reduce(lambda x,y: x + [y] + y.hypernyms(), 
+				synsets, [])
+			wordnet_features = reduce(
+				lambda x,y: x + [l.name for l in y.lemmas], hypernyms, []
+			)
+
+			features = features + wordnet_features
+
+			# add the synsets and hypernyms (by name) to the as-found words
+			features = Counter(features)
+
+			# get rid of empty-string features
+			del features['']
+
+			class_name = row[2]
+			return_data.append((class_name, dict(features)))
+
+		self.cache('hypernyms', return_data)
+
+		return return_data
+
+
+	def get_class_counts(self, use_cache=True):
+
+		# check for cached data first
+		if use_cache and self.check_cache('class_counts'):
+			return self.get_cache('class_counts')
+
+		class_counts = defaultdict(lambda: Counter())
+		for row in self.data:
+
+			class_name = row[2]
+
+			# update word counts for the class from which this example came
+			class_counts[class_name].update(self.NON_ALPHANUM.split(row[1]))
+
+		# delete the empty-string counts
+		for class_name in class_counts:
+			del class_counts[class_name]['']
+
+		self.cache('class_counts', class_counts)
+
+		return class_counts
+
+
+	def tficf(self, use_cache=True):
+		'''
+		the features are weighted frequency counts, where the frequency
+		count is weighted by inverse class frequency.
+		'''
+
+		# check for cached data first
+		if use_cache and self.check_cache('tficf'):
+			return self.get_cache('tficf')
+
+		# first, we must find the counts on a per-class basis
+		class_counts = self.get_class_counts(use_cache)
+
+		# now we are ready to compute icf weighting factors
+		# first, assemble the entire vocabulary
+		vocab = set()
+		for class_name in class_counts:
+			vocab.update(class_counts[class_name].keys())
+		
+		print 'total size of vocabulary: %d words.' % len(vocab)
+
+		icf_scores = {}
+		for word in vocab:
+
+			# how many categories have this word?
+			presence = [
+				bool(word in class_counts[cn] and class_counts[cn][word]>0) 
+				for cn in class_counts
+			]
+			icf_scores[word] = sum(presence)
+
+		self.cache('icf_scores', icf_scores)
+
 
 
 	def as_frequencies(self, use_cache=True):
